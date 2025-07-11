24866a948c8fb6bf49d452d026a294324468dcd4
Shivank Shukla <118785192+Shivank0101@users.noreply.github.com>
2025-07-11 15:30:26 +0530

    Update server.js For Production

--- backend/server.js
+++ backend/server.js
@@ -32,20 +32,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
 }));
 
+// ✅ Middleware
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
-
-// ✅ This line handles preflight requests
-app.options("*", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
